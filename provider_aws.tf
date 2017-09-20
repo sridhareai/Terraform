d469a4c32f4452363bc1c95b3b9539680f1fea40
sridhareai <31828194+sridhareai@users.noreply.github.com>
2017-09-19 19:49:09 -0600

    Delete provider_aws.tf

--- provider_aws.tf
+++
@@ -1,7 +0,0 @@
-
-# Specify the provider and access details will take from credentials file of your local and make sure the profile to be default
-provider "aws" {
-  shared_credentials_file = "./credentials"
-  profile                 = "default"
-  region                  = "${var.region}"
-}
